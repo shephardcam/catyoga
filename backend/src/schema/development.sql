002d46c2363e2fba8b2fd07b815786d03ba47fdc
Victoria Amirov <vika.amir20@gmail.com>
2023-11-18 15:07:35 -0800

    add db

---
+++ backend/src/schema/development.sql
@@ -0,0 +1,21 @@
+INSERT INTO USER(ID, FULLNAME,EMAIL)
+
+VALUES
+ (1, 'John Doe', 'john.doe@example.com'),
+  (2, 'Alice Wonderland', 'alice.wonder@example.com'),
+  (3, 'Sita Dennis', 'sita.dennis@example.com'),
+  (4, 'Sasha Mateo', 'sasha.mateo@example.com'),
+
+
+  INSERT INTO YOGA_CLASSES (ID, CLASS_NAME, CAPACITY) VALUES
+(1, 'Beginner Yoga', true),
+(2, 'Advanced Yoga', true),
+(3, 'Hatha Yoga', true),
+(4, 'Vinyasa Flow', true);
+
+INSERT INTO CLASS_INFO (ID, CLASS_NAME, DATE, PRICE, DESCRIPTION, START_TIME, AVAILABLE_SPOTS) VALUES
+(1, 'Yoga Basics', '2023-12-01', 25.00, 'Introduction to foundational yoga poses.', '09:00:00', 15),
+(2, 'Power Yoga', '2023-12-02', 30.00, 'Energetic and dynamic yoga practice.', '18:30:00', 20),
+(3, 'Meditation Session', '2023-12-03', 15.00, 'Guided meditation for relaxation.', '11:00:00', 10),
+(4, 'Hatha Yoga', '2023-12-04', 28.00, 'Gentle yoga with a focus on alignment.', '17:00:00', 18),
+(5, 'Vinyasa Flow', '2023-12-05', 27.50, 'Continuous flow between poses for flexibility.', '19:00:00', 22);
