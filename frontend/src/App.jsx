f38194edab5549fd6ee30ceca7776430aaaaaf8e
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-11-18 18:14:28 -0500

    organized routes for react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.scss";
 import React from "react";
 import TopNav from './components/TopNav';
-import Login from "./components/Login";
+
+import { Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <h1 className='App-link'> Cat Yoga</h1> */}
+      <Route exact path="/" Component={Home}/>
+      <Route exact path="/about" Component={About}/>
+      <Route exact path="/faq" Component={FAQ}/>
+      <Route exact path="/booking" Component={booking}/>
+
+      
       <TopNav/>
-      <Login />
+     
     </div>
   );
 }
--- frontend/src/components/topNav.jsx
+++ frontend/src/components/topNav.jsx
@@ -6,12 +6,14 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import '../styles/TopNav.scss';
+import { Link } from 'react-router-dom';
 
 function TopNav() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
+    <Box sx={{ flexGrow: 1 }}  >
+      <AppBar position="static" >
+        <Toolbar className='TopNav-bar'>
           <IconButton
             size="large"
             edge="start"
@@ -19,10 +21,10 @@ function TopNav() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <MenuIcon  />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+          <Typography variant="h2" component="div" sx={{ flexGrow: 1 }}>
+            Cat Yoga
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
