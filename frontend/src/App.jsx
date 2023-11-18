ea2b2d1ae56828aadf49e74690a9e0d38fd708ed
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-11-17 20:29:09 -0500

    organized the setup for inputing the frontend data

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App-header">
+      <h1 className='App-link'> Cat Yoga</h1>
+    </div>
+  );
+}
+
+export default App;
