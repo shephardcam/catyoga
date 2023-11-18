27a55694364eb57d7ee1263865ec8e30879a3518
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-11-18 16:31:36 -0500

    testing out some features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.scss';
 import React from 'react';
+// import topNav from './components/topNav';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App-header">
-      <h1 className='App-link'> Cat Yoga</h1>
+    <div className="App">
+      {/* <h1 className='App-link'> Cat Yoga</h1> */}
+      {/* <topNav/> */}
+      <Login/>
     </div>
   );
 }
