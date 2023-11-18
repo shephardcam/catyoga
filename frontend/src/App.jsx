c59d7b96ea86b61debb3efbc1f8b4f5cc4e71901
@OneJuicyDoor <shephardcam@gmail.com>
2023-11-18 13:11:07 -0400

    create nav bar component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import TopNav from './components/TopNav'
+
+function App() {
+  return (
+    <div className="App">
+      <TopNav/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+    </div>
+  );
+}
+
+export default App;
