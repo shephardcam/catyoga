7907fc91960cc201326a81b68b50b102ca22ed0e
Jonas Cura <jonasm.cura@gmail.com.com>
2023-11-21 21:43:40 -0700

    connect backend data to frontend and display hard-coded data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,40 @@
+import React, { useEffect, useState } from 'react';
 
 import './App.scss';
 import TopNav from './components/TopNav'
 
 function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, []);
+
+
+
   return (
     <div className="App">
       <TopNav/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+      {/* testing backend data fetching */}
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ): (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+
     </div>
   );
 }
