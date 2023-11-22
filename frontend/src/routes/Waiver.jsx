6cdee4149d808abf784c1de151b4cb2a4d7827ea
@OneJuicyDoor <shephardcam@gmail.com>
2023-11-22 14:54:00 -0400

    creat waiver component (not on a route yet)

--- frontend/src/routes/Waiver.jsx
+++ frontend/src/routes/Waiver.jsx
@@ -1,12 +1,11 @@
-
-import './App.scss';
 import TopNav from './components/TopNav'
+import Waiver from './components/Waiver';
 
 function App() {
   return (
-    
     <div className="App">
       <TopNav/>
+      <Waiver/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import TopNav from '../components/TopNav'
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = ({ photos, topics, setIsModalOpen, handlePhotoClick, likes, toggleLike }) => {
+const HomeRoute = () => {
   return (
     <div className="home-route">
       <TopNav/>
