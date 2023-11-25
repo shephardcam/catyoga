8667860e22251003fa73bb7e407fbec932f2b505
@OneJuicyDoor <shephardcam@gmail.com>
2023-11-25 16:34:30 -0400

    made the catyoga FAQ page

--- frontend/src/pages/FAQPage.jsx
+++ frontend/src/pages/FAQPage.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
-
+import '../styles/FAQ.scss';
 
 const FAQPage = () => {
   return (
     <div >
-      <h1> Welcome to FAQ page</h1>
-    </div>
+      <h1 className="faq-header"  >  Frequently Asked Questions:</h1>
+ 
+<body>
+
+<h2 class="faq-question">1. What is CatYoga?</h2>
+    <p class="faq-answer">CatYoga is a unique and purrfect fusion of yoga and feline companionship. It's a yoga experience where you can bring your beloved cat to the class, creating a harmonious balance of relaxation and feline fun.</p>
+
+    <h2 class="faq-question">2. How does CatYoga work?</h2>
+    <p class="faq-answer">CatYoga classes are conducted just like traditional yoga classes, but with the delightful addition of our feline friends. Participants bring their cats to enjoy the practice together, enhancing the overall yoga experience.</p>
+
+    <h2 class="faq-question">3. Is CatYoga suitable for all cats?</h2>
+    <p class="faq-answer">While many cats enjoy CatYoga, it's essential to consider your cat's temperament. Cats that are calm, social, and adaptable tend to thrive in this environment. It's advisable to consult with your veterinarian if you have concerns about your cat's suitability for CatYoga.</p>
+
+    <h2 class="faq-question">4. What should I bring to a CatYoga class?</h2>
+    <p class="faq-answer">Aside from your usual yoga gear, bring your cat's favorite blanket or mat, treats, and any toys that might help keep them engaged. Don't forget to bring a signed copy of our CatYoga waiver, ensuring a stress-free experience for everyone.</p>
+
+    <h2 class="faq-question">5. Can I participate if I don't have a cat?</h2>
+    <p class="faq-answer">Absolutely! CatYoga is designed for both cat owners and cat enthusiasts. You can still enjoy the unique atmosphere of our classes, surrounded by fellow cat lovers and their adorable feline companions.</p>
+
+    <h2 class="faq-question">6. Are there specific poses tailored for cats in CatYoga?</h2>
+    <p class="faq-answer">While the primary focus is on traditional yoga poses for humans, CatYoga classes may incorporate cat-friendly stretches and movements. Our certified instructors are experienced in creating an inclusive environment that accommodates both human and feline participants.</p>
+
+    <h2 class="faq-question">7. What safety measures are in place for CatYoga?</h2>
+    <p class="faq-answer">Safety is our top priority. Participants are required to read and sign our CatYoga waiver, acknowledging potential risks. Instructors are trained to guide classes with the well-being of both humans and cats in mind. Participants are encouraged to keep their cats on leashes or in carriers when not actively participating in poses.</p>
+
+    <h2 class="faq-question">8. Can I bring more than one cat to CatYoga?</h2>
+    <p class="faq-answer">While we love our feline friends, it's recommended to bring only one cat per participant to ensure a calm and enjoyable experience for everyone in the class.</p>
+
+    <h2 class="faq-question">9. Is there an age limit for participants?</h2>
+    <p class="faq-answer">CatYoga is open to participants of all ages! Whether you're a seasoned yogi or a beginner, our classes cater to a diverse range of individuals looking to share a unique and relaxing experience with their cats.</p>
+
+    <h2 class="faq-question">10. How do I sign up for a CatYoga class?</h2>
+    <p class="faq-answer">Right here on our website you can view the class schedule and sign up for upcoming sessions. Don't forget to read and sign the CatYoga waiver before attending your first class. We look forward to seeing you and your furry friend on the mat!</p>
+
+</body>
+   </div>
   );
 };
 
