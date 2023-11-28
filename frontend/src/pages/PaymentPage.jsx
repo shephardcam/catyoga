26b86250bb47b53b4338ed76aca5ea620b50385d
@OneJuicyDoor <shephardcam@gmail.com>
2023-11-27 20:58:54 -0400

    crated basic payment

--- frontend/src/pages/PaymentPage.jsx
+++ frontend/src/pages/PaymentPage.jsx
@@ -1,10 +1,16 @@
+import React from 'react';
 import { CardElement } from '@stripe/react-stripe-js';
+import '../styles/payment.scss';
 
 const PaymentPage = () => {
   return (
-    <form>
-      <CardElement />
-      <button type="submit">Submit</button>
+    <form className="payment-form">
+      <div className="card-element">
+        <CardElement />
+      </div>
+      <button type="submit" className="submit-button">
+        Submit
+      </button>
     </form>
   );
 };
