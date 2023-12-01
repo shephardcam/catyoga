58ff7e3dfecb5e6fc1eda1a4ac330ccdc4e14e72
@OneJuicyDoor <shephardcam@gmail.com>
2023-12-01 13:30:42 -0400

    add missing imports

--- frontend/src/components/YogaListItem.jsx
+++ frontend/src/components/YogaListItem.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import '../styles/yogaListItem.scss';
 import BookingButton from './BookingButton';
+import { useYogaContext } from './YogaContext';
+import { Link } from 'react-router-dom';
 
 const YogaListItem = ({ className, price, details, duration, classDay, startTime, isMaxCapacity, maxCapacity }) => {
   const { setYogaData } = useYogaContext();
 
-    return (
-      <div className="Yoga-list-item">
-        <h3>{className}</h3>
-        <p>price: {price}</p>
-        <p>Is Max Capacity: {maxCapacity}</p>
-        <p>details: {details}</p>
-        <p>duration: {duration}</p>
-        <h2>
-          {classDay} {startTime} {isMaxCapacity}
-        </h2>
-
+  
   return (
     <div className="YogaListItem">
       <h2>{classDay}{startTime} {isMaxCapacity}</h2>
