2dd99def2a06490abfe73b7ba6c99dd138c7b85d
Victoria Amirov <vika.amir20@gmail.com>
2023-11-30 21:04:20 -0800

    fixed the isMaxCapacity

--- frontend/src/components/YogaListItem.jsx
+++ frontend/src/components/YogaListItem.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import '../styles/yogaListItem.scss';
-import BookingButton from './BookingButton'
-
+import BookingButton from './BookingButton';
 
 class YogaListItem extends React.Component {
-
   render() {
     const { className, price, maxCapacity, details, duration, classDay, startTime, isMaxCapacity } = this.props;
 
     return (
       <div className="Yoga-list-item">
-        <h3> {className}</h3>
+        <h3>{className}</h3>
         <p>price: {price}</p>
         <p>Is Max Capacity: {maxCapacity}</p>
         <p>details: {details}</p>
         <p>duration: {duration}</p>
-        <h2>{classDay} {startTime} {isMaxCapacity}</h2>
+        <h2>
+          {classDay} {startTime} {isMaxCapacity}
+        </h2>
 
-           <BookingButton className={className} classDay={details} startTime={price} />
+        {isMaxCapacity ? (
+          <p>Class is Full</p>
+        ) : (
+          <BookingButton className={className} classDay={details} startTime={price} />
+        )}
       </div>
     );
   }
