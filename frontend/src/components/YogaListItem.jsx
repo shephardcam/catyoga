a310c734c66c23f275a767592b05b0cd0aef2481
Victoria Amirov <vika.amir20@gmail.com>
2023-11-30 20:22:09 -0800

    added bookingbutton component

--- frontend/src/components/YogaListItem.jsx
+++ frontend/src/components/YogaListItem.jsx
@@ -1,32 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import '../styles/yogaListItem.scss';
+import BookingButton from './BookingButton'
 
 
 class YogaListItem extends React.Component {
+
   render() {
-    const {className, price, maxCapacity, details, duration, classDay, startTime, isMaxCapacity } = this.props;
+    const { className, price, maxCapacity, details, duration, classDay, startTime, isMaxCapacity } = this.props;
 
     return (
-      <div className="YogaListItem">
+      <div className="Yoga-list-item">
         <h3>Yoga Class</h3>
         <p>name: {className}</p>
         <p>price: {price}</p>
         <p>Is Max Capacity: {maxCapacity}</p>
         <p>details: {details}</p>
         <p>duration: {duration}</p>
-        <h2>{classDay}{startTime} {isMaxCapacity}</h2>
-        
-        {maxCapacity ? (
-          <p>Full</p>
-        ) : (
-          <Link to={`/waiver?name=${className}&price=${price}&details=${details}`}>
-            Purchase
-          </Link>
-        )}
+        <h2>{classDay} {startTime} {isMaxCapacity}</h2>
+
+           <BookingButton className={className} classDay={details} startTime={price} />
       </div>
     );
   }
 }
-
 
 export default YogaListItem;
