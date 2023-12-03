185617f18b952c730a72cdfac92bd79a6e35f20e
@OneJuicyDoor <shephardcam@gmail.com>
2023-12-03 19:10:14 -0400

    made loging out bring you to /

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Login = ({ onLogin }) => {
   const [email, setEmail] = useState('');
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const ProfilePage = ({ onLogout }) => {
   return (
     <div >
       <h1> Welcome to Profile page</h1>
-      <div className="nav-item"><button onClick={onLogout}>Logout</button></div>
+      <div className="nav-item"><button onClick={onLogout}>  
+       <Link to="/">
+      Logout
+      </Link></button></div>
     </div>
   );
 };
