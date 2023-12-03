f37995312fa73795877affb18ff6b893b63e944a
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-12-03 17:23:41 -0500

    no change

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,30 +5,30 @@ import { Button } from "../components/Button";
 
 const HomePage = () => {
   return (
-    <div className="cat-container">
-      <video src="/videos/video-1.1.mp4" autoPlay loop muted />
-      {/* <img src={`${process.env.PUBLIC_URL} /images/image-1.png`} /> */}
-      <h1>RELAXATION AWAITS</h1>
-      <p>
-        "Unwind and rejuvenate, <br /> with a delightful yoga session
-        accompanied by playful cats, <br /> a perfect blend to soothe your body
-        and mind!" <i className="fa-solid fa-cat" />
-      </p>
+      <div className="cat-container">
+        <video src="/videos/video-1.1.mp4" autoPlay loop muted />
+        {/* <img src={`${process.env.PUBLIC_URL} /images/image-1.png`} /> */}
+        <h1>RELAXATION AWAITS</h1>
+        <p>
+          "Unwind and rejuvenate, <br /> with a delightful yoga session
+          accompanied by playful cats, <br /> a perfect blend to soothe your
+          body and mind!" <i className="fa-solid fa-cat" />
+        </p>
 
-      <div className="cat-btns">
-        <Button
-          className="btns"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
-        >
-          Book Now <i className="fa-solid fa-cat" />
-        </Button>
-      </div>
+        <div className="cat-btns">
+          <Button
+            className="btn"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+          >
+            Book Now <i className="fa-solid fa-cat" />
+          </Button>
+        </div>
 
       {/* <img className="catSleep" src={`${process.env.PUBLIC_URL} /images/catSleep.ipeg`} />
       <img src={`${process.env.PUBLIC_URL} /images/cat yoga pose.webp`} />
       <img src={`${process.env.PUBLIC_URL} /images/cat yoga.jpeg`} />
-      <img src={`${process.env.PUBLIC_URL} /images/cat.jpeg`} />      */}
+    <img src={`${process.env.PUBLIC_URL} /images/cat.jpeg`} />      */}
     </div>
   );
 };
