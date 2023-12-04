92e7ee11a9c1ac6f81f5a77dd9d5d2f0c69ad0ad
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-12-04 16:08:46 -0500

    working on scss of about page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-
-
+import '../styles/AboutPage.scss';
+import Content from '../components/Content';
 
 const AboutPage = () => {
   return (
-    <div >
-     
-
-      <h1> About Us</h1>
-     
-
+    <div className='cat-container' >
+      <video src="/videos/video-3.mp4" autoPlay loop muted />
+   
+      <Content /> 
+      
     </div>
   );
 };
 
 export default AboutPage;
+
+
