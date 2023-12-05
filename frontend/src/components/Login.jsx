26ba33db75274fc4eff0981e3e4a02c2930b2051
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-12-04 19:07:45 -0500

    made scss for login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "../styles/Login.scss";
 
 const Login = ({ onLogin }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post('/api/login', { email, password });
+      const response = await axios.post("/api/login", { email, password });
       const { user } = response.data;
 
       // Set cookie with user info
@@ -19,31 +20,51 @@ const Login = ({ onLogin }) => {
       onLogin(user);
 
       // Redirect to homepage
-      navigate('/');
-
+      navigate("/");
     } catch (error) {
-      console.error('Login failed:', error);
+      console.error("Login failed:", error);
     }
   };
 
   return (
-    <div>
+    <div className="main">
+    <div className="container">
       <h2>Login</h2>
       <form>
-        <label>
-          Email:
-          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+        <label className="input-box">
+          <h6>Email:</h6>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <i class="fa-solid fa-user"></i>
         </label>
+
         <br />
-        <label>
-          Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+        <label className="input-box">
+          <h6>Password:</h6>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <i class="fa-solid fa-lock"></i>
         </label>
+
         <br />
-        <button type="button" onClick={handleLogin}>
+
+        <button className="btn-btn" type="button" onClick={handleLogin}>
           Login
         </button>
+
+        <div className="register-link" >
+          Don't have an account, Register!
+        </div>
       </form>
+    </div>
     </div>
   );
 };
--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import './styles/TopNavBar.scss';
-import { Link } from 'react-router-dom';
-import { Button } from './Button';
+import React, { useState, useEffect } from "react";
+import "./styles/TopNavBar.scss";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
 
 const TopNav = ({ user }) => {
-
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
 
@@ -12,7 +11,7 @@ const TopNav = ({ user }) => {
   const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
-    if(window.innerWidth <= 960) {
+    if (window.innerWidth <= 960) {
       setButton(false);
     } else {
       setButton(true);
@@ -20,39 +19,79 @@ const TopNav = ({ user }) => {
   };
 
   useEffect(() => {
-    showButton()
-  },[])
+    showButton();
+  }, []);
 
-  window.addEventListener('resize', showButton)
+  window.addEventListener("resize", showButton);
 
   return (
     <>
-    <nav className="topNav">
-      <div className="topNav-container">
-        <Link to="/" className='topNav-logo' onClick={closeMobileMenu}>CATYOGA <i className="fa-solid fa-cat"/></Link>
-      <div className='menu-icon' onClick={handleClick}><i className={click ? 'fas fa-times' : 'fas fa-bars'}/></div>
-      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-        <li className='nav-item'><Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link></li>
-        <li className='nav-item'><Link to='/about' className='nav-links' onClick={closeMobileMenu}>About</Link></li>
-        <li className='nav-item'><Link to='/FAQ' className='nav-links' onClick={closeMobileMenu}>FAQ</Link></li>
-        <li><Link to='/yoga-class-info' className='nav-links-mobile' onClick={closeMobileMenu}>Book Now</Link></li>
-        <li><Link to='/login' className='nav-links-mobile' onClick={closeMobileMenu}>Login</Link></li> 
-        {/* Handles display if user logged in */}
-        {user ? (
-          // if user logged in
-          <>
-            <li className='nav-item'><Link to='/profile' className='nav-links' onClick={closeMobileMenu}>Profile</Link></li>
-          </>
-        ) : (
-          //  if user not logged in
-          <div className="nav-item"><Link to='/login' className='nav-links'>Login</Link></div>
-        )}
-      </ul>
+      <nav className="topNav">
+        <div className="topNav-container">
+          <Link to="/" className="topNav-logo" onClick={closeMobileMenu}>
+            CATYOGA <i className="fa-solid fa-cat" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/FAQ" className="nav-links" onClick={closeMobileMenu}>
+                FAQ
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/yoga-class-info"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Book Now
+              </Link>
+            </li>
+      
+            {/* Handles display if user logged in */}
+            {user ? (
+              // if user logged in
+              <>
+                <li className="nav-item">
+                  <Link
+                    to="/profile"
+                    className="nav-links"
+                    onClick={closeMobileMenu}
+                  >
+                    Profile
+                  </Link>
+                </li>
+              </>
+            ) : (
+              //  if user not logged in
+              <li>
+                <Link to="/login" className="nav-links-mobile" onClick={closeMobileMenu}>
+                  Login
+                </Link>
+              </li>
+            )}
+          </ul>
 
-      {button && <Button className="btn--outline" buttonStyle='btn--outline'>Book Now</Button>}
-      {/* {button && <Button className="btn--outline" buttonStyle='btn--outline'>Login</Button>} */}
-      </div>
-    </nav>
+          {button && (
+            <Button className="btn--outline" buttonStyle="btn--outline">
+              Book Now
+            </Button>
+          )}
+          {/* {button && <Button className="btn--outline" buttonStyle='btn--outline'>Login</Button>} */}
+        </div>
+      </nav>
     </>
   );
 };
