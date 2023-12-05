0414884f60a053ecc9c89d00d71ef44774fb2699
Jonas Cura <jonasm.cura@gmail.com.com>
2023-12-05 12:46:54 -0700

    remake profile page backend elements from merge overwrite

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,28 +1,67 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useYogaContext } from '../components/YogaContext';
-import '../styles/ProfilePage.scss'
 
-const ProfilePage = ({ onLogout }) => {
-  const { state } = useYogaContext();
-  console.log('Context State:', state);
-  const { className, details, startTime, classDay, user } = state;
+const ProfilePage = ({ onLogout, user, yogaClasses, yogaClassInfo }) => {
+
+  //Get registered_classes
+  const [allRegisteredClassIds, setAllRegisteredClassIds] = useState([]);
+
+  useEffect(() => {
+    fetch('./api/registered-classes')
+    .then((response) => response.json())
+    .then((data) => {
+      setAllRegisteredClassIds(data);
+    })
+    .catch((error) => {
+    console.error("Error fetching registered classes data:", error);
+    })
+  }, []);
+
+  // console.log('allRegisteredClassIds', allRegisteredClassIds)
+  
+  // Select only classIDs that match user.id
+  const userRegisteredClassIds = allRegisteredClassIds.filter((userClassId) => userClassId.user_id === user.id);
+  // console.log('userRegisteredClasses', userRegisteredClassIds)
+
+  // Select IDs from yoga_classes
+  const userRegisteredClasses = userRegisteredClassIds.map((userClass) => userClass.yoga_class_id);
+  // Select only classes with userID matching user.id
+  const userYogaClasses = yogaClasses.filter((yogaClass) => userRegisteredClasses.includes(yogaClass.id));
+  // console.log('userRegisteredClassesInYoga', userYogaClasses);
+
+  // Render user's registered classes
+  const renderUserClasses = () => {
+    if (userYogaClasses.length === 0) {
+      return <p>No registered classes.</p>;
+    }
+
+    return (
+      <ul>
+        {userYogaClasses.map((classItem) => (
+          <li key={classItem.id}>
+            Day: {classItem.class_day}, 
+            Class: {yogaClassInfo[classItem.yoga_class_info_id-1].class_name},
+            Details: {yogaClassInfo[classItem.yoga_class_info_id-1].details},
+            Time: {classItem.start_time},
+            Duration: {yogaClassInfo[classItem.yoga_class_info_id-1].duration},
+            Price: ${yogaClassInfo[classItem.yoga_class_info_id-1].price}
+          </li>
+        ))}
+      </ul>
+    );
+  };
 
   return (
-    <body>
-      <div className="container">
-        <h1>Welcome</h1>
-        <div className="nav-item">
-          <button onClick={onLogout}>
-            <Link to="/">Logout</Link>
-          </button>
-        </div>
+    <div >
+      <h1> Welcome to Profile page</h1>
+      <div className="nav-item"><button onClick={onLogout}>  
+       <Link to="/">
+      Logout
+      </Link></button>
       </div>
-      <div className="next-class">
-        Your next class is {className} {details}
-        <p>on {classDay} at {startTime}</p>
-      </div>
-    </body>
+      {/* Render the user's registered classes */}
+      {renderUserClasses()}
+    </div>
   );
 };
 
