9d85a866d23217b09f187de628536e876ed610ae
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-12-05 13:51:25 -0500

    removed console log

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import "../styles/Login.scss";
+import "../styles/login.scss";
 
 const Login = ({ onLogin }) => {
   const [email, setEmail] = useState("");
