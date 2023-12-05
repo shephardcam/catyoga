2cdede29c6ea0cf7b604e23d4a851db407b5ee26
Jonas Cura <jonasm.cura@gmail.com.com>
2023-12-05 02:14:54 -0700

    add copies of changed code in case of merge conflicts

---
+++ frontend/src/components/YogaContext copy.jsx
@@ -0,0 +1,71 @@
+import React, { createContext, useContext, useReducer } from 'react';
+
+// Define the initial state
+const initialState = {
+  className: '',
+  price: '',
+  details: '',
+  id: null,
+  // add selected classId
+  selectedYogaClass:null, // changed code
+};
+
+// Define the actions
+const SET_YOGA_DATA = 'SET_YOGA_DATA';
+
+// add selected yogaClass
+const SET_SELECTED_YOGA_CLASS = 'SET_SELECTED_YOGA_CLASS'; // changed code
+
+// Define the reducer
+const yogaReducer = (state, action) => {
+  switch (action.type) {
+    case SET_YOGA_DATA:
+      return {
+        ...state,
+        className: action.payload.className,
+        price: action.payload.price,
+        details: action.payload.details,
+        id: action.payload.id
+      };
+    // add new case 
+    case SET_SELECTED_YOGA_CLASS: // changed code
+        return{
+          ...state,
+          selectedYogaClass: action.payload,
+        };
+    default:
+      return state;
+  }
+};
+
+// Create the context
+const YogaContext = createContext();
+
+// Create the provider
+export const YogaProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(yogaReducer, initialState);
+
+  const setYogaData = (data) => {
+    dispatch({ type: SET_YOGA_DATA, payload: data });
+  };
+
+  // add new logic
+  const setSelectedYogaClass = (yogaClass) => { // changed code
+    dispatch({ type: SET_SELECTED_YOGA_CLASS, payload: yogaClass });
+  };
+
+  return (
+    <YogaContext.Provider value={{ state, setYogaData, setSelectedYogaClass }}> 
+      {children}
+    </YogaContext.Provider>
+  );
+};
+
+// Create a custom hook for using the context
+export const useYogaContext = () => {
+  const context = useContext(YogaContext);
+  if (!context) {
+    throw new Error('useYogaContext must be used within a YogaProvider');
+  }
+  return context;
+};
---
+++ frontend/src/components/YogaList copy.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import YogaListItem from './YogaListItem';
+import '../styles/yogaList.scss';
+
+
+const YogaList = ({ yogaClasses, user, yogaClassInfo }) => {
+  console.log('yoooooooooooooo', yogaClasses)
+  console.log('class info', yogaClassInfo)
+
+  // yogaClasses.map((yogaClass) => (
+  //   console.log(yogaClass.yoga_class_info_id)
+  // ))
+
+  return (
+    <div>
+      <h2 class_name ="titel-yoga">Yoga Classes </h2>
+      <ul >
+        {yogaClasses.map((yogaClassData) => (
+          <YogaListItem
+            // from yoga_classes
+            yogaClass={yogaClassData}
+            id={yogaClassData.id}
+            classDay={yogaClassData.class_day}
+            startTime={yogaClassData.start_time}
+            isMaxCapacity={yogaClassData.is_max_capacity}
+            classInfoId={yogaClassData.yoga_class_info_id}
+
+            // from yoga_class_info@yoga_classes
+            className={yogaClassInfo[yogaClassData.yoga_class_info_id-1].class_name}
+            price={yogaClassInfo[yogaClassData.yoga_class_info_id-1].price}
+            maxCapacity={yogaClassInfo[yogaClassData.yoga_class_info_id-1].max_capacity}
+            details={yogaClassInfo[yogaClassData.yoga_class_info_id-1].details}
+            duration={yogaClassInfo[yogaClassData.yoga_class_info_id-1].duration}
+            
+            //other
+            user={user}
+          />
+        ))}
+      </ul>
+      <p></p>
+    </div>
+  );
+};
+
+export default YogaList;
